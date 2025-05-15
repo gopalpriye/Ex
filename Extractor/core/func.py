6c10e488a2a620da7b5aa54b3ecedb7ce1d29372
gopalpriye <govindkumaryadav8651@gmail.com>
2025-05-15 12:20:15 +0530

    Update func.py

--- Extractor/core/func.py
+++ Extractor/core/func.py
@@ -30,10 +30,10 @@ async def subscribe(app, message):
       try:
          user = await app.get_chat_member(update_channel, message.from_user.id)
          if user.status == "kicked":
-            await message.reply_text("Sorry Sir, You are Banned. Contact My Support Group @DevsOops")
+            await message.reply_text("Sorry Sir, You are Banned. Contact My Support Group @SEM2JOB")
             return 1
       except UserNotParticipant:
-         await message.reply_photo(photo="https://img.freepik.com/premium-photo/king-cobra-snake-cool-character-background-hd-wallpaper_862489-10700.jpg?w=360",caption=script.FORCE_MSG.format(message.from_user.mention), reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🤖 ᴊᴏɪɴ ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ 🤖", url=f"{url}")]]))
+         await message.reply_photo(photo="https://te.legra.ph/file/785a5f16b49e0c1a86f91-d1f8030af62adc39d2.jpg",caption=script.FORCE_MSG.format(message.from_user.mention), reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🤖 ᴊᴏɪɴ ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ 🤖", url=f"{url}")]]))
          return 1
       except Exception:
          await message.reply_text("Something Went Wrong. Contact My Support Group")
