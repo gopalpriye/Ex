6a4ef68c46eaab8fb2c4092ef5bd9a93cee1ede3
gopalpriye <govindkumaryadav8651@gmail.com>
2025-05-15 12:37:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ OWNER_ID = int(getenv("OWNER_ID", "7836088695"))
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5637668210").split()))
 MONGO_URL = getenv("MONGO_DB", "mongodb+srv://MRDAXX:MRDAXX@mrdaxx.prky3aj.mongodb.net/?retryWrites=true&w=majority")
 
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002385747075"))
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002489642006"))
 PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", "-1002385747075"))
 
 """
@@ -27,7 +27,7 @@ API_ID = int(os.environ.get("API_ID", "18116358"))
 # ------------------------------------------------
 API_HASH = os.environ.get("API_HASH", "80e7597f27a57df271dfc500120d4ea9")
 # ----------------D--------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7594405866:AAF527H4zrRubxeLfS_mAfOIgu_idqG9MsM")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7527954976:AAG18eVvsB3BezEzo4e3UYeiYmSRzL-Osfk")
 # -----------------A-------------------------------
 BOT_USERNAME = os.environ.get("BOT_USERNAME")
 # ------------------X------------------------------
@@ -36,7 +36,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "7836088695"))
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5637668210").split()))
 # ------------------------------------------------
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002385747075"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002489642006"))
 # ------------------------------------------------
 MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody")
 # -----------------------------------------------
